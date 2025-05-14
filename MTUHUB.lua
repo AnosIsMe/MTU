0491c887406c117ce4274a681fc8347ad0cf8bb9
AnosIsMe <acc80noi@gmail.com>
2025-05-14 16:19:10 +0700

    Add files via upload

--- MTUHUB.lua
+++ MTUHUB.lua
@@ -1,4 +1,4 @@
 while true do
-    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("AddSpeed"):FireServer()
-wait(0;1)
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("AddSpeed"):FireServer()
+wait(0.1)
 end
